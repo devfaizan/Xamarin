e1a74ad132fc32bb354bbf2d4e2a75edf7982dd5
Faizan <Faizan@hotmail.com>
2017-12-18 14:26:15 +0500

    Brought conversion functionality from Backend to UI

--- PDFToImage.Droid/MainActivity.cs
+++ PDFToImage.Droid/MainActivity.cs
@@ -11,18 +11,28 @@ using System.IO;
 using Java.Lang;
 using Android.Graphics;
 using PDFToImage.Droid.Scripts;
+using PDFToImage.Droid.Models;
 
 namespace PDFToImage.Droid
 {
     [Activity(Label = "PDFToImage.Droid", MainLauncher = true)]
     public class MainActivity : Activity, IWebViewClientActivity, IValueCallback
     {
+        #region --- Controls ---
         WebView webview;
         WebView webview2;
         LinearLayout mainLayout;
         ProgressDialog progressDialog;
-        readonly string url = "https://docs.google.com/gview?embedded=true&url=http://altwebtest01.zaifworks.com/CertDownload.aspx?code=cert-10075-99ZH";
+        Button btnConvert;
+        EditText txtUrl;
+        #endregion
 
+        #region --- Fields ---
+        readonly string gViewURL = "https://docs.google.com/gview?embedded=true&url=";
+        string url;
+        #endregion
+
+        #region --- Activity Events/Methods ---
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -31,9 +41,19 @@ namespace PDFToImage.Droid
 
             webview = new WebView(this);
             mainLayout = FindViewById<LinearLayout>(Resource.Id.mainLayout);
-            mainLayout.AddView(webview, new LinearLayout.LayoutParams(1354, 100000));
-
+            txtUrl = FindViewById<EditText>(Resource.Id.editText1);
+            btnConvert = FindViewById<Button>(Resource.Id.button1);
             progressDialog = new ProgressDialog(this);
+
+            txtUrl.Text = "http://altwebtest01.zaifworks.com/CertDownload.aspx?code=cert-10075-99ZH";
+            btnConvert.Click += BtnConvert_Click;
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            url = gViewURL + txtUrl.Text;
+
+            mainLayout.AddView(webview, new LinearLayout.LayoutParams(1354, 100000));
 
             if (webview != null)
             {
@@ -47,11 +67,21 @@ namespace PDFToImage.Droid
             }
         }
 
+        /// <summary>
+        /// Invokes when OnError is called on web view having ExtendedWebViewClient
+        /// </summary>
+        /// <param name="view"></param>
+        /// <param name="url"></param>
         public void OnError()
         {
             //throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Invokes when OnWebViewPageFinshed is called on web view having ExtendedWebViewClient
+        /// </summary>
+        /// <param name="view"></param>
+        /// <param name="url"></param>
         public async void OnWebViewPageFinshed(WebView view, string url)
         {
             if (view.Progress == 100)
@@ -62,6 +92,54 @@ namespace PDFToImage.Droid
             }
         }
 
+        /// <summary>
+        /// Invokes when EvaluateJavascript gets called
+        /// </summary>
+        /// <param name="result"></param>
+        public async void OnReceiveValue(Java.Lang.Object result)
+        {
+            try
+            {
+                var json = result as Java.Lang.String;
+                var str = result.ToString();
+                var jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonReturn>(str);
+                if (!jsonObject.loaded && webview2 == null)
+                    await LoadPDF();
+                else
+                {
+                    if (webview2 == null)
+                    {
+                        mainLayout.RemoveView(webview);
+                        webview2 = webview;
+                        webview2.SetBackgroundColor(Color.White);
+                        webview2.Settings.JavaScriptEnabled = true;
+                        webview2.Settings.SetPluginState(WebSettings.PluginState.On);
+                        mainLayout.AddView(webview2, new LinearLayout.LayoutParams(1354, Convert.ToInt32(jsonObject.result)));
+                        await Task.Delay(TimeSpan.FromSeconds(15));
+                        await CheckIfPDFRendered();
+
+                    }
+                    else if (jsonObject.loaded)
+                    {
+                        var image = webview2.ToImage();
+                        image.SaveAndAddToGallery(Android.OS.Environment.DirectoryDownloads, Guid.NewGuid().ToString() + ".png", this);
+                        Toast.MakeText(this, "Image created added to library", ToastLength.Short).Show();
+                    }
+                    else
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                        await CheckIfPDFRendered();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                System.Diagnostics.Debugger.Log(0, "re", ex.ToString());
+            }
+        }
+        #endregion
+
+        #region --- Helper Methods ---
         private async Task LoadPDF()
         {
             await Task.Delay(TimeSpan.FromSeconds(1));
@@ -84,55 +162,7 @@ namespace PDFToImage.Droid
         {
             webview.EvaluateJavascript("javascript: (function(){return { loaded: " + GoogleGViewHelperScripts.ScriptGViewLoaded + ", result: " + GoogleGViewHelperScripts.ScriptGetClientHeight + " }})()", this);
         }
-
-        public async void OnReceiveValue(Java.Lang.Object result)
-        {
-            try
-            {
-                var json = result as Java.Lang.String;
-                var str = result.ToString();
-                JsonReturn jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonReturn>(str);
-                if (!jsonObject.loaded && webview2 == null)
-                    await LoadPDF();
-                else
-                {
-                    if (webview2 == null)
-                    {
-                        mainLayout.RemoveViewAt(0);
-                        webview2 = webview;
-                        webview2.SetBackgroundColor(Color.White);
-                        webview2.Settings.JavaScriptEnabled = true;
-                        webview2.Settings.SetPluginState(WebSettings.PluginState.On);
-                        mainLayout.AddView(webview2, new LinearLayout.LayoutParams(1354, Convert.ToInt32(jsonObject.result)));
-                        await Task.Delay(TimeSpan.FromSeconds(15));
-                        await CheckIfPDFRendered();
-
-                    }
-                    else if (jsonObject.loaded)
-                    {
-                        var image = webview2.ToImage();
-                        image.SaveAndAddToGallery(Android.OS.Environment.DirectoryDownloads, Guid.NewGuid().ToString() + ".png", this);
-                        Toast.MakeText(this, "Image created added to library", ToastLength.Short);
-                    }
-                    else
-                    {
-                        await Task.Delay(TimeSpan.FromSeconds(1));
-                        await CheckIfPDFRendered();
-                    }
-                }
-            }
-            catch (System.Exception ex)
-            {
-                System.Diagnostics.Debugger.Log(0, "re", ex.ToString());
-            }
-        }
-
-
-        public class JsonReturn
-        {
-            public bool loaded;
-            public string result;
-        }
+        #endregion
     }
 }
 
